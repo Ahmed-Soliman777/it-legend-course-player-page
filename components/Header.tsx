16bd7d96d0cdc157fd08728147fa34c416e53015
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-05 14:59:32 +0300

    Create header component

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Player page component
+ * for displaying current page for users
+ */
+
+const Header = () => {
+  return (
+    <header>
+      
+    </header>
+  )
+}
+
+export default Header

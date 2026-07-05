a38d2556a1aee4252b8205aef8fc67244e8f00ec
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-05 17:24:40 +0300

    Create VideoPlayer component

---
+++ components/video_player/VideoPlayer.tsx
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @title video player component
+ * @description this component for playing videos
+ * @style VideoPlayer.css
+ */
+
+
+import './VideoPlayer.css'
+
+const VideoPlayer = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default VideoPlayer

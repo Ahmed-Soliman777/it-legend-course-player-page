765005cc4ed513561a5ea8f6a9dbe7f078d72f32
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-06 00:47:17 +0300

    Create Comments component

---
+++ components/comments/Comments.tsx
@@ -0,0 +1,16 @@
+/**
+ * @title Comments component
+ * @description display comments for a course
+ * @props 
+ */
+
+
+const Comments = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Comments

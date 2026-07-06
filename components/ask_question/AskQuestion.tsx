7a35869cb227b0d00a09d8cd2823c9808eee9958
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-06 23:47:19 +0300

    Create AskQuestion component

---
+++ components/ask_question/AskQuestion.tsx
@@ -0,0 +1,16 @@
+/**
+ * @title ask question component
+ * @description this component will pop up when user clicks on ask icon and save their data
+ */
+
+import React from 'react'
+
+const AskQuestion = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AskQuestion

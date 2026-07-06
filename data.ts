00d1fcada8ed5797160912b3b0facd4abc2b0f7d
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-06 16:25:20 +0300

    Create data file to store dummy data & add comments dummy data

---
+++ data.ts
@@ -0,0 +1,23 @@
+/**
+ * @title data file
+ * @description store dummy data here
+ */
+
+import { CommentsType } from "./types";
+
+export const CommmentsData: CommentsType[] = [
+  {
+    image: "./profile.jpg",
+    name: "Insert your name here",
+    date: "2026-10-1",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam dolor expedita eligendi quae? Minus, iste delectus vero quidem aliquam, qui, harum hic tempore velit labore iusto dolore laborum ex quis eligendi iure consequatur eius esse eveniet facere possimus. Facilis dolorum iusto odio tenetur consequatur quos ab praesentium, quo ipsa eius!",
+  },
+  {
+    image: "./profile.jpg",
+    name: "Insert your name here",
+    date: "2026-10-1",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam dolor expedita eligendi quae? Minus, iste delectus vero quidem aliquam, qui, harum hic tempore velit labore iusto dolore laborum ex quis eligendi iure consequatur eius esse eveniet facere possimus. Facilis dolorum iusto odio tenetur consequatur quos ab praesentium, quo ipsa eius!",
+  },
+];

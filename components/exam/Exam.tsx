48dae7b437fe49aa74eee0d8c61367c573755ad4
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-08 01:00:46 +0300

    create exam component

---
+++ components/exam/Exam.tsx
@@ -0,0 +1,14 @@
+/**
+ * @title exam component
+ * @description this component is responsible for course exam for each level
+ */
+
+const Exam = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Exam

2248c8574aa6aa471da25e4b12df9d64ffed966e
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-08 01:05:59 +0300

    create exam card component

---
+++ components/exam_card/ExamCard.tsx
@@ -0,0 +1,14 @@
+/**
+ * @title exam card component
+ * @descriton here question and options will displayed
+ */
+
+const ExamCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExamCard

d9e0070517964b46f26587c491813504f1025783
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-07 14:40:11 +0300

    Create CourseTopics component

---
+++ components/course_topics/CourseTopics.tsx
@@ -0,0 +1,17 @@
+/**
+ * @title course topics component
+ * @descrtion here user will get all topics about course with progress bar
+ */
+
+import './CourseTopics.css';
+
+const CourseTopics = () => {
+    return (
+        <section className="container">
+            <h2>Topics For This Course</h2>
+
+        </section>
+    );
+};
+
+export default CourseTopics;

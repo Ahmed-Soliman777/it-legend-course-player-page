10e4310795ef7cf99f35d2bb50984f66e83b0874
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-07 23:28:40 +0300

    Add Comments - CourseMaterials - CourseTopics - VideoPlayer components to video player page & add page.module.css style file

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 /**
  * 
  * @description it legend player page
- * @components Header - 
+ * @components Header - Comments - CourseMaterials - CourseTopics - VideoPlayer
  */
 
 
@@ -11,23 +11,34 @@ import CourseTopics from "@/components/course_topics/CourseTopics";
 import Header from "@/components/header/Header";
 import VideoPlayer from "@/components/video_player/VideoPlayer";
 
+import styles from './page.module.css'
+
 export default function Home() {
   return (
-    <>
-      {/* player page header component*/}
-      <Header />
+    <div className={styles.it_player_video}>
+      <div className={styles.mainContent}>
+        {/* player page header component*/}
+        <Header />
 
-      {/* vide player component*/}
-      <VideoPlayer />
+        {/* vide player component*/}
+        <VideoPlayer />
 
-      {/* course material component */}
-      <CourseMaterials />
+        {/* course material component */}
+        <CourseMaterials />
 
-      {/* course topics component */}
-      <CourseTopics />
 
-      {/* comments component */}
-      <Comments />
-    </>
+        {/* course topics component */}
+        <div className={styles.smallTopics}>
+          <CourseTopics />
+        </div>
+
+        {/* comments component */}
+        <Comments />
+      </div>
+
+      <aside className={styles.largeTopics}>
+        <CourseTopics />
+      </aside>
+    </div>
   );
 }

4f17c8f0be0c774e6558fd2eb52fb1e78ea9b1b0
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-07 12:48:25 +0300

    Create AskQuestion component structure

--- components/ask_question/AskQuestion.tsx
+++ components/ask_question/AskQuestion.tsx
@@ -1,16 +1,54 @@
 /**
  * @title ask question component
- * @description this component will pop up when user clicks on ask icon and save their data
+ * @description ask question pop up for make users add their inquiries about the course
+ * @props { isOpen, value, onClose, onChange }
  */
 
-import React from 'react'
 
-const AskQuestion = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+"use client";
 
-export default AskQuestion
+import "./AskQuestion.css";
+
+import { AskQuestionProps } from "@/types";
+
+const AskQuestion = ({ isOpen, value, onClose, onChange }: AskQuestionProps) => {
+
+    if (!isOpen) {
+        return null;
+    }
+
+    return (
+        <div className="ask-popup">
+            <div
+                className="overlay"
+                onClick={onClose}></div>
+
+            <div className="ask-popup-card" onClick={(event) => event.stopPropagation()}>
+                <button className="close-btn" onClick={onClose} type="button">
+                    ×
+                </button>
+
+                {/* pop up title */}
+
+                <h3 className="course-name">Course Name Shown Here</h3>
+                <h3 className="popup-title">Ask A Question</h3>
+
+                {/* question form */}
+
+                <textarea
+                    name="ask-area"
+                    id="ask-area-id"
+                    placeholder="Ask a question"
+                    value={value}
+                    onChange={(event) => onChange(event.target.value)}
+                ></textarea>
+
+                <button className="ask-btn" onClick={onClose} type="button">
+                    Submit
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default AskQuestion;

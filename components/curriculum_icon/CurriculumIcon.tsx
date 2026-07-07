d55f5c5b970f6a960b95ca656c38204702a943dd
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-07 23:35:14 +0300

    Update componet to controll ask question & leader pop ups

--- components/curriculum_icon/CurriculumIcon.tsx
+++ components/curriculum_icon/CurriculumIcon.tsx
@@ -1,31 +1,80 @@
 /**
- * 
+ *
  * @title Curriculum icon compoent
  * @description for navegating to comment, asking question and leaderboard
- */
+ * @components AskQuestion Leaderboard
+*/
+"use client";
 
+import { useEffect, useState } from "react";
 
-import './CurriculumIcon.css'
-
-import { FaQuestion, FaComment } from "react-icons/fa";
+import { FaComment, FaQuestion } from "react-icons/fa";
 
 import { MdLeaderboard } from "react-icons/md";
 
+import AskQuestion from "@/components/ask_question/AskQuestion";
+
+import Leaderboard from "@/components/leaderboard/Leaderboard";
+
+import "./CurriculumIcon.css";
+
 
 const CurriculumIcon = () => {
+
+    // local states
+
+    const [isAskPopupOpen, setIsAskPopupOpen] = useState(false);
+    const [isLeaderboardPopupOpen, setIsLeaderboardPopupOpen] = useState(false);
+
+    const [askQuestionValue, setAskQuestionValue] = useState("");
+
+
+    // use Effects for renders
+
+    // set ask question session storage
+    useEffect(() => {
+        sessionStorage.setItem("ask-question", askQuestionValue);
+    }, [askQuestionValue]);
+
+
+
     return (
         <>
-            <span title='Add new comment' className="curriculum-icon">
+            <span title="Add new comment" className="curriculum-icon">
                 <FaComment />
             </span>
-            <span title='Ask a question' className="curriculum-icon">
+
+            <span
+                title="Ask a question"
+                className="curriculum-icon"
+                onClick={() => setIsAskPopupOpen(true)}
+            >
                 <FaQuestion />
             </span>
-            <span title='go to leaderboard' className="curriculum-icon">
+
+            <span
+                title="go to leaderboard"
+                className="curriculum-icon"
+                onClick={() => setIsLeaderboardPopupOpen(true)}
+            >
                 <MdLeaderboard />
             </span>
-        </>
-    )
-}
 
-export default CurriculumIcon
+            {/* ask question pop up */}
+            <AskQuestion
+                isOpen={isAskPopupOpen}
+                value={askQuestionValue}
+                onChange={setAskQuestionValue}
+                onClose={() => setIsAskPopupOpen(false)}
+            />
+
+            {/* leaderboard pop up */}
+            <Leaderboard
+                isOpen={isLeaderboardPopupOpen}
+                onClose={() => setIsLeaderboardPopupOpen(false)}
+            />
+        </>
+    );
+};
+
+export default CurriculumIcon;

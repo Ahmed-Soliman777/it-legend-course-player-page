2a0173bae4921ba6bf0414229785c390e915b270
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-07-08 14:59:40 +0300

    Add exam card structure

--- components/exam_card/ExamCard.tsx
+++ components/exam_card/ExamCard.tsx
@@ -3,12 +3,48 @@
  * @descriton here question and options will displayed
  */
 
-const ExamCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+'use client'
+
+import './ExamCard.css';
+
+
+import { ExamCardProps } from '@/types';
+
+
+const ExamCard = ({
+    question,
+    questionIndex,
+    selectedValue,
+    onAnswerSelect,
+}: ExamCardProps) => {
+    return (
+        <div className='exam-card-shell'>
+            <div className='exam-card'>
+                <div className='exam-card-header'>
+                    <span className='exam-card-badge'>{questionIndex + 1}.</span>
+                </div>
+
+                <p className='exam-question'>
+                    {question.question}
+                </p>
+
+                <div className='options-list'>
+                    {question.options.map((option) => (
+                        <label className='option-item' key={option}>
+                            <input
+                                type='radio'
+                                name={`question-${questionIndex}`}
+                                value={option}
+                                checked={selectedValue === option}
+                                onChange={() => onAnswerSelect(option)}
+                            />
+                            <span>{option}</span>
+                        </label>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ExamCard
